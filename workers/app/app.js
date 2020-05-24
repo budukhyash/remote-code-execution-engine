7555e9b6c7ea7761dc12571115996f5b68363006
budukhyash <budukhyash@gmail.com>
2020-05-25 01:08:32 +0530

    consumer service

---
+++ workers/app/app.js
@@ -0,0 +1,142 @@
+var fs  = require('fs');
+const amqp = require('amqp-connection-manager');
+const redis = require('redis');
+const bail = require('bail');
+const rimraf=require("rimraf");
+
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+
+client.on('error', (err) => {
+    console.log("Error " + err)
+}); 
+
+const extensions = {
+    "cpp":"cpp",
+    "c": "c",
+    "java":"java",
+    "python3":"py"
+};
+
+function runCode(apiBody,ch,msg)
+{   
+
+    client.set(apiBody.folder.toString(),'{"status":"Processing"}');
+    const { exec } = require('child_process');   
+    var output;
+    var command = 'python3 run.py ../temp/' + apiBody.folder +'/source.' + extensions[apiBody.lang] + ' ' + apiBody.lang + ' '  + apiBody.timeOut;  
+
+    fs.writeFile('/temp/' + apiBody.folder+  '/output.txt','',function(err){
+        if(err)
+        console.log(err);
+        else
+        console.log("Output.txt created");
+    });
+
+    exec(command, (err, stdout, stderr) => 
+    {
+        if (err) {
+            ch.ack(msg);   
+            console.log(err);
+        }else
+        {   
+            fs.readFile('/temp/' + apiBody.folder+  '/output.txt', 'utf8', function(err, contents) {
+                
+                if(err)
+                console.log(err);
+                output=contents;
+
+                let result =
+                {
+                    'output':output,
+                    'stderr':`${stderr}`,
+                    'stdout':`${stdout}`,
+                    'submission_id':apiBody.folder
+                }
+                
+                console.log(result);
+                console.log(apiBody);
+                rimraf("../temp/"+apiBody.folder,function(err){
+                    if(err)
+                    console.log(err);
+                     else
+                    console.log("DELETED TEMP FOLDER");
+                });
+
+                client.setex(apiBody.folder.toString(), 3600, JSON.stringify(result));
+                ch.ack(msg);
+
+            });
+        }
+    });
+}
+
+
+function createFiles(apiBody,ch,msg)
+{
+
+    fs.mkdir('/temp/'+ apiBody.folder,function(err)
+    {   
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            fs.writeFile('/temp/'+ apiBody.folder +'/input.txt', apiBody.input, function (err) {
+                if (err) 
+                    return console.log(err);
+                else
+                {
+                    fs.writeFile("/temp/" + apiBody.folder + "/source." + extensions[apiBody.lang], apiBody.src, function (err) {
+                        if (err) 
+                            console.log(err);
+                        else    
+                        {
+                            runCode(apiBody,ch,msg);
+                        }       
+                });
+                }
+            });
+
+        }
+    });
+
+}
+
+var QUEUE_NAME = 'judge'
+var onMessage = function(data) {
+
+    let message = JSON.parse(data.content.toString());
+    console.log(message);
+    createFiles(message,channelWrapper,data);    
+}
+
+// Create a connetion manager
+var connection = amqp.connect(['amqp://rabbitmq:5672']);
+
+connection.on('connect', function() {
+    console.log('Connected!');
+});
+connection.on('disconnect', function(err) {
+    console.log('Disconnected.', err.stack);
+});
+
+// Set up a channel listening for messages in the queue.
+var channelWrapper = connection.createChannel({
+    setup: function(channel) {
+        // `channel` here is a regular amqplib `ConfirmChannel`.
+        return Promise.all([
+            channel.assertQueue(QUEUE_NAME, {durable: true}),
+            channel.prefetch(1),
+            channel.consume(QUEUE_NAME, onMessage)
+        ]);
+    }
+});
+
+channelWrapper.waitForConnect()
+.then(function() {
+    console.log("Listening for messages");
+});
---
+++ workers/app/run.py
@@ -0,0 +1,68 @@
+import os, filecmp ,sys
+
+codes = {200:'success',404:'file not found',400:'error',408:'timeout'}
+
+def compile(file,lang):
+
+    if(lang =='python3'):
+        return 200
+
+    if (os.path.isfile(file)):
+        if lang=='c':
+            os.system('gcc ' + file)
+        elif lang=='cpp':
+            os.system('g++ ' + file)
+        elif lang=='java':
+            os.system('javac ' + file)
+        if (os.path.isfile('a.out')) or (os.path.isfile('main.class')):
+            return 200
+        else:
+            return 400
+    else:
+        return 404
+
+def run(file,input,timeout,lang):
+    cmd='sudo -u judge '
+    if lang == 'java':
+        cmd += 'java main'
+    elif lang=='c' or lang=='cpp':
+        cmd += './a.out'
+    elif lang=='python3':
+        cmd += 'python3 '+ file
+
+    r = os.system('timeout '+timeout+' '+cmd+' < '+input + ' > '+testout)
+
+    if r==0:
+        return 200
+    elif r==31744:
+        print("TIMEOUT")
+        return 408
+    else:
+        print("ERROR")
+        return 400
+
+def match(output):
+    if os.path.isfile('out.txt') and os.path.isfile(output):
+        b = filecmp.cmp('out.txt',output)
+        os.remove('out.txt')
+        return b
+    else:
+        return 404
+
+params=sys.argv
+file = params[1].split('/')[3]
+path = os.getcwd()
+folder = params[1].split('/')[2]
+path = '../temp/' +folder +'/'
+
+os.chdir(path)
+lang = params[2]
+timeout = params[3]
+
+testin =  "input.txt"
+testout =  "output.txt"
+
+status=compile(file,lang)
+if status ==200:
+    status=run(file,testin,timeout,lang)
+
